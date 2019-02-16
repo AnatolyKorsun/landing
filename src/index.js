ffb7cd6b9bbedb9a6c8c98342cd3562e9c48217a
AnatolyKorsun <korsoon@i.ua>
2019-02-17 00:10:30 +0200

    add showmore portfolio

--- src/index.js
+++ src/index.js
@@ -4,62 +4,74 @@ const galleryItems = [
   {
     preview: "/src/images/preview-1.jpg",
     fullview: "/src/images/fullview-1.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-02/",
+    alt: "HTML,tag,attribute,elements,Git."
   },
   {
     preview: "/src/images/preview-2.jpg",
     fullview: "/src/images/fullview-2.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-03/",
+    alt: "CSS,visual formatting,alignment,background."
   },
   {
     preview: "/src/images/preview-3.jpg",
     fullview: "/src/images/fullview-3.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-04/",
+    alt: "color,alpha chanel,linear-gradient,::before,::after,float,filter"
   },
   {
     preview: "/src/images/preview-4.jpg",
     fullview: "/src/images/fullview-4.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-05/",
+    alt: "position,z-index,HTML5 media"
   },
   {
     preview: "/src/images/preview-5.jpg",
     fullview: "/src/images/fullview-5.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-06/",
+    alt:"Flexbox,shasow,transform,transition"
   },
   {
     preview: "/src/images/preview-6.jpg",
     fullview: "/src/images/fullview-6.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-07/",
+    alt: "font,absolute and relative units,table,form"
   },
   {
     preview: "/src/images/preview-7.jpg",
     fullview: "/src/images/fullview-7.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-08/",
+    alt: "Responsive,ChromeDevTols,Mobile-first CSS"
   },
   {
     preview: "/src/images/preview-8.jpg",
     fullview: "/src/images/fullview-8.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-09/",
+    alt: "Grid,Bootstrap"
   },
   {
     preview: "/src/images/preview-9.jpg",
     fullview: "/src/images/fullview-9.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-10/",
+    alt: "Retina,SVG,BEM"
   },
   {
     preview: "/src/images/preview-10.jpg",
     fullview: "/src/images/fullview-10.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/html-css/module-11/",
+    alt: "SASS,variables,Nesting,&,Data Types,Mixin"
   },
   {
     preview: "/src/images/preview-11.jpg",
     fullview: "/src/images/fullview-11.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/js/module-07/",
+    alt: "DOM"
   },
   {
     preview: "/src/images/preview-12.jpg",
     fullview: "/src/images/fullview-12.jpg",
-    alt: "alt text 1"
+    lowsrc:"https://anatolykorsun.github.io/goit-fe-course/js/module-08/",
+    alt: "event,eventListener"
   },
   {
     preview: "/src/images/preview-13.jpg",
@@ -95,13 +107,13 @@ const galleryItems = [
 ];
 let galeryItem = [0,1,2,3];
 
+const showMore = (event) =>{
+  window.open(`${event.target.lowsrc}`,'_blank');
+}
 
 const hideButton = () =>{
   const buttonForvard = document.querySelector(".btn--next");
   const buttonBack = document.querySelector(".btn--prev");
-  console.log(galeryItem[galeryItem.length-1]);
-  console.log(Number(galleryItems.length-1));
-  console.log(galeryItem[galeryItem.length-1] === Number(galleryItems.length-1))
   if(galeryItem[galeryItem.length-1] === Number(galleryItems.length-1)){
     buttonForvard.style.visibility = "hidden";
   };
@@ -122,16 +134,17 @@ const moveBack = () =>{
     });
 create();
 }
-
-function createFullviewImage(dataFullview) {
+let dataFullview = galleryItems[0].fullview
+let lowsrc = galleryItems[0].lowsrc
+  
+function createFullviewImage(dataFullview,lowsrc) {
+  
   return `<div class="fullview">
-      <img src="${dataFullview}" alt="alt text 1"></div>`;
+      <img class="large" src="${dataFullview}" lowsrc="${lowsrc}" alt="alt text 1"></div>`;
 };
 
 function createCard(galleryItems) {
-  return `<li><img src=${galleryItems.preview} data-fullview=${
-    galleryItems.fullview
-    } alt="${galleryItems.alt}">
+  return `<li><img src=${galleryItems.preview} lowsrc= ${galleryItems.lowsrc} data-fullview=${galleryItems.fullview} alt="${galleryItems.alt}">
     </li>`;
 };
 
@@ -144,17 +157,20 @@ function createGallery(){
   return result;
 }
 
-let dataFullview = galleryItems[0].fullview
+
+
 
 function create() {
   const list = document.querySelector(".js-image-gallery");
-  list.innerHTML = createFullviewImage(dataFullview) + createGallery(galleryItems);
+  list.innerHTML = createFullviewImage(dataFullview, lowsrc) + createGallery(galleryItems);
 
   const gallery = document.querySelector(".preview");
   gallery.addEventListener("click", onGalleryClick);
 
   hideButton();
- 
+ const fullviewImage = document.querySelector(".large");
+ fullviewImage.addEventListener("click",showMore);
+
   const preview = document.querySelector(".js-prev");
   preview.addEventListener("click",moveBack);
   
@@ -167,6 +183,7 @@ function onGalleryClick(event) {
   const nodeName = target.nodeName;
   if (nodeName !== 'IMG') return;
   dataFullview = target.getAttribute('data-fullview');
+  lowsrc = event.target.getAttribute('lowsrc');
   create()
 }
 
